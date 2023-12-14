8024ef055036afec6e51a0b5b5a0a5f39003828d
ajayrai03 <ajayrai1012@gmail.com>
2023-12-14 08:46:57 +0530

    getElementsByTagName done

--- dom.js
+++ dom.js
@@ -29,8 +29,8 @@
 
 //GETELEMENTSBYCLASSNAME
 
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
 // console.log(items[1]);
 // items[1].textContent='Hello 2';
 // items[1].style.fontWeight='bold';
@@ -42,8 +42,23 @@ console.log(items);
 //     items[i].style.backgroundColor='#f4f4f4';
 // }
 
-items[2].style.backgroundColor='green';
+// items[2].style.backgroundColor='green';
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
+
+// GETELEMENTSBYTAGNAME
+
+var li=document.getElementsByTagName('li');
+console.log(li);
+console.log(li[1]);
+li[1].textContent='Hello 2';
+li[1].style.fontWeight='bold';
+li[1].style.backgroundColor='yellow';
+
+
+for(var i=0;i<li.length;i++){
+    li[i].style.backgroundColor='#f4f4f4';
 }
+
